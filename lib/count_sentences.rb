6ff588325e94079946e75d077577e0231171bf95
Salem Asrat <salemasrat@gmail.com>
2019-11-02 17:51:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+     self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?")
   end
 
   def exclamation?
-
+      self.end_with?("!")
   end
 
   def count_sentences
-
+      self.count_sentences
   end
+  def count_sentences_words
+      binding.pry
 end
